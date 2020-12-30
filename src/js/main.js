9b9039aea271ff252b01ea64a206d01eab30f35a
YamaD <yamad@xn--d-8o2b.com>
2020-12-30 22:27:35 +0900

    お

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,31 @@
-import cjpDict from './cjp.json'
+import cjpDictWord from './cjp.word.json'
+import cjpDictKana from './cjp.kana.json'
+import cjpDictKanji from './cjp.kanji.json'
+import cjpDictEmoji from './cjp.emoji.json'
 
-const copy = element => {
-    if (!element.value) return
-    element.select()
-    document.execCommand('copy')
-    return
-}
-const gen = (text, dictList) => {
+const replaceWithDict = (text, dictList) => {
     let translateStr = text
     for (const i in dictList) {
         const regex = new RegExp(i, 'g')
         translateStr = translateStr.replace(regex, dictList[i])
     }
     return translateStr
+}
+
+const translate = text => {
+    let translateStr = text
+    translateStr = replaceWithDict(translateStr, cjpDictWord)
+    translateStr = replaceWithDict(translateStr, cjpDictKana)
+    translateStr = replaceWithDict(translateStr, cjpDictKanji)
+    translateStr = replaceWithDict(translateStr, cjpDictEmoji)
+    return translateStr
+}
+
+const copy = element => {
+    if (!element.value) return
+    element.select()
+    document.execCommand('copy')
+    return
 }
 
 const inputBox = document.getElementById('inputBox')
@@ -22,12 +35,12 @@ const onClickFunc = (id, func) =>
     document.getElementById(id).addEventListener('click', func)
 
 onClickFunc('doGen', () => {
-    outputBox.value = gen(inputBox.value, cjpDict)
+    outputBox.value = translate(inputBox.value)
 })
 onClickFunc('doCopy', () => {
     copy(outputBox)
 })
 onClickFunc('doGenAndCopy', () => {
-    outputBox.value = gen(inputBox.value, cjpDict)
+    outputBox.value = translate(inputBox.value)
     copy(outputBox)
 })

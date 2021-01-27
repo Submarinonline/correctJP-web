a380ccd75bf3d91b4c9e61cf1eeac547a1ae7709
YamaD <yamad@xn--d-8o2b.com>
2021-01-28 01:29:47 +0900

    追加

---
+++ api/raw.js
@@ -0,0 +1,21 @@
+const { translate } = require('cjp')
+
+module.exports = (req, res) => {
+    try {
+        let reqData
+        if (req.body.t) {
+            reqData = req.body.t
+        } else if (req.query.t) {
+            reqData = req.query.t
+        } else {
+            throw 'No target text found'
+        }
+        try {
+            res.status(200).send(translate(reqData))
+        } catch (err) {
+            res.status(500).send('Server Error')
+        }
+    } catch (err) {
+        res.status(400).send('Bad API Call')
+    }
+}

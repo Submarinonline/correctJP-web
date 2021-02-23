2680ee4e16a9c62849d4e969a8b079251a6f657a
YamaD <yamad@xn--d-8o2b.com>
2021-02-23 19:04:26 +0900

    お

--- api/version.js
+++ api/version.js
@@ -1,6 +1,5 @@
 const p = require('../node_modules/cjp/package.json')
 
-console.log(p.version)
 module.exports = (req, res) => {
-            res.status(200).send(p.version)
+    res.status(200).send(p.version)
 }
